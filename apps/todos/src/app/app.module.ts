eef2153aa98a902fcd7746eda36bf8b8b0b8a4b3
James Wright <jcwright04@gmail.com>
2019-10-07 21:34:14 -0500

    scaffold ngrx store

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { UiModule } from '@tutorials/ui';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +18,19 @@ import { UiModule } from '@tutorials/ui';
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     HttpClientModule,
     UiModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true
+        }
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreRouterConnectingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
